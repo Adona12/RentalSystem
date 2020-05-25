a3ada38cea70b992e37821d46849459763904ac5
Adona Tesfaye <adonatesfaye99@gmail.com>
2020-05-25 12:48:10 +0300

    update to login

--- Admin/AddDriver.php
+++ Admin/AddDriver.php
@@ -9,10 +9,10 @@ echo '<div class="alert">
     }
 ?>
         <div class="row">
-            <form class="col s12"  method="post" action="DriverRegister.php">
+            <form class="col s12"  method="post" action="DriverRegister.php" enctype="multipart/form-data">
               <div class="row">
                 <div class="input-field col s6">
-                  <input placeholder="Placeholder" id="first_name" name="fname" type="text" class="validate">
+                  <input placeholder="First Name" id="first_name" name="fname" type="text" class="validate">
                   <label for="first_name">First Name</label>
                 </div>
                 <div class="input-field col s6">
@@ -20,16 +20,11 @@ echo '<div class="alert">
                   <label for="last_name">Last Name</label>
                 </div>
               </div>
+              
               <div class="row">
                 <div class="input-field col s12">
-                  <input disabled value="I am not editable" id="disabled" type="text" class="validate">
-                  <label for="disabled">Disabled</label>
-                </div>
-              </div>
-              <div class="row">
-                <div class="input-field col s12">
-                  <input id="password" type="password" class="validate">
-                  <label for="password">Password</label>
+                  <input id="phone" type="text" class="validate">
+                  <label for="phone">Phone number</label>
                 </div>
               </div>
               <div class="row">
@@ -39,14 +34,16 @@ echo '<div class="alert">
                 </div>
               </div>
               <div class="row">
-                <div class="col s12">
-                  This is an inline input field:
-                  <div class="input-field inline">
-                    <input id="email_inline" type="email" class="validate">
-                    <label for="email_inline">Email</label>
-                    <span class="helper-text" data-error="wrong" data-success="right">Helper text</span>
-                  </div>
-                </div>
+              <div class="file-field input-field">
+      <div class="btn">
+        <span>File</span>
+        <input type="file" name="file">
+      </div>
+      <div class="file-path-wrapper">
+        <input class="file-path validate" type="text">
+      </div>
+    </div>
+   
               </div>
               <button id="sbtn" class="btn waves-effect waves-light" type="submit" name="Register">Submit
     <i class="material-icons right">send</i>
